b5a255942e44c3e1f35deb6508535b63a82526fc
Jessica Triana <jessica.triana.tx@gmail.com>
2020-01-18 22:55:46 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,8 @@
 def run_guessing_game
   random_number = rand(6) + 1
   puts "Guess a number between 1 and 6"
-  user_response = gets.chomp.to_i
+  user_response = gets.chomp
+  user_response.to_i
   
   if user_response == "exit"
     puts "Goodbye!"
